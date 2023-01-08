f43ffd97caa720ed7658157004a2919ade8e1b4a
Alexey Makeev <makeevhome@mail.ru>
2023-01-08 23:31:32 +0300

    fix: error class name popup

--- src/view/popup-view.js
+++ src/view/popup-view.js
@@ -221,13 +221,12 @@ const createPopupTemplate = (movie, comments) => {
       </div>
 
         <section class="film-details__controls">
-          <button type="button" class="film-details__control-button film-details__control-button--watchlist ${watchlist ? 'film-card__controls-item--active' : ''}" id="watchlist" name="watchlist">
+          <button type="button" class="film-details__control-button film-details__control-button--watchlist ${watchlist ? 'film-details__control-button--active' : ''}" id="watchlist" name="watchlist">
           ${watchlist ? 'Added' : 'Add'} to watchlist
           </button>
-          <button type="button" class="film-details__control-button film-details__control-button--watched ${alreadyWatched ? 'film-card__controls-item--active' : ''}" id="watched" name="watched">
+          <button type="button" class="film-details__control-button ${alreadyWatched ? 'film-details__control-button--active' : ''} film-details__control-button--watched" id="watched" name="watched">
           ${alreadyWatched ? 'Already watched' : 'Not Viewed'}</button>
-          <button type="button" class="film-details__control-button film-details__control-button--favorite
-          ${favorite ? 'film-card__controls-item--active' : ''}" id="favorite" name="favorite">
+          <button type="button" class="film-details__control-button film-details__control-button--favorite ${favorite ? 'film-details__control-button--active' : ''}" id="favorite" name="favorite">
           ${favorite ? 'Added' : 'Add'} to favorites
           </button>
         </section>
